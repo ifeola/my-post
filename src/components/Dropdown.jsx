ba9c783e57700f03a118b544fe328e048f48751b
Arogunmasa Abayomi <nomorelimit11@gmail.com>
2024-11-22 14:24:30 +0100

    Added skeleton to the UI

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -12,25 +12,25 @@ const Dropdown = ({ className }) => {
   return (
     <div
       className={`absolute top-14 right-0 w-64 bg-background-clr border border-transparent rounded-xl transition-[height] ${className}`}>
-      <div className="p-4 flex items-center gap-2">
+      <div className="flex items-center gap-2 p-4">
         <Profile className="w-12 h-12" />
         <div>
           <h4 className="font-bold">
             Abayomi{" "}
-            <span className="px-1 py-0 bg-teal-700 border-2 border-teal-950 text-teal-950 rounded-lg">
+            <span className="px-1 py-0 bg-teal-700 border-2 rounded-lg border-teal-950 text-teal-950">
               PRO
             </span>
           </h4>
-          <span className="font-semibold text-sm text-gray-400">
+          <span className="text-sm font-semibold text-gray-400">
             abayomia@gmail.com
           </span>
         </div>
       </div>
-      <ul className="border-y border-y-gray-700 p-2">
+      <ul className="p-2 border-y border-y-gray-700">
         <li>
           <Link
             to=""
-            className="flex items-center gap-2 px-2 py-2 rounded-lg hover:bg-gray-800 transition-colors">
+            className="flex items-center gap-2 px-2 py-2 transition-colors rounded-lg hover:bg-gray-800">
             <Settings />
             <span>Profile Settings</span>
           </Link>
@@ -38,7 +38,7 @@ const Dropdown = ({ className }) => {
         <li>
           <Link
             to=""
-            className="flex items-center gap-2 px-2 py-2 rounded-lg hover:bg-gray-800 transition-colors">
+            className="flex items-center gap-2 px-2 py-2 transition-colors rounded-lg hover:bg-gray-800">
             <HelpCircle />
             <span>Help Center</span>
           </Link>
@@ -46,7 +46,7 @@ const Dropdown = ({ className }) => {
         <li>
           <Link
             to=""
-            className="flex items-center gap-2 px-2 py-2 rounded-lg hover:bg-gray-800 transition-colors">
+            className="flex items-center gap-2 px-2 py-2 transition-colors rounded-lg hover:bg-gray-800">
             <Wallet />
             <span>Billings</span>
           </Link>
@@ -54,13 +54,13 @@ const Dropdown = ({ className }) => {
         <li>
           <Link
             to=""
-            className="flex items-center gap-2 px-2 py-2 rounded-lg hover:bg-gray-800 transition-colors">
+            className="flex items-center gap-2 px-2 py-2 transition-colors rounded-lg hover:bg-gray-800">
             <PackagePlus />
             <span>Upgrade Plan</span>
           </Link>
         </li>
       </ul>
-      <button className="p-4 flex items-center gap-2">
+      <button className="flex items-center gap-2 p-4">
         <LogOut />
         <span>Sign Out</span>
       </button>
